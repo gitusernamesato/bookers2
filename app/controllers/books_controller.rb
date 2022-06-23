3595c853dcbd33d17377940815b8444f87a0263a
EC2 Default User <shinya.sin09@gmail.com>
2022-06-23 10:15:26 +0000

    [finish]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,8 @@ class BooksController < ApplicationController
     if @book.save
        redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
+       @books = Book.all
+       @user = current_user
        render :index
     end
   end
